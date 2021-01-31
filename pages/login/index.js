c5cba0953b366e84b917f1d7ee6857d97120c859
李雪静 <517701261@qq.com>
2021-01-31 16:19:58 +0800

    微信小程序与vant weapp

---
+++ pages/login/index.js
@@ -0,0 +1,74 @@
+// pages/login/index.js
+import { login } from '../../utils/asyncWx'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  async getUserInfo(e) {
+    // 获取用户信息
+    const { encryptedData, iv, rawData, signature } = e
+    // 获取小程序登录成功后的code
+    const { code } = await login()
+    console.log(code)
+    
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/user/history/index.js
@@ -0,0 +1,107 @@
+// pages/user/history/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    products: [
+      {
+        picture: '',
+        position: "南锣鼓巷附近",
+        title: '锦江之星（北京王府井店）',
+        score: '4.6',
+        number: '5000+',
+        price: '360',
+        red: '新人红包',
+        id: 1
+      },
+      {
+        picture: '',
+        position: "南锣鼓巷附近",
+        title: '锦江之星（北京王府井店）',
+        score: '4.6',
+        number: '5000+',
+        price: '360',
+        red: '新人红包',
+        id: 2
+      },
+      {
+        picture: '',
+        position: "南锣鼓巷附近",
+        title: '锦江之星（北京王府井店）',
+        score: '4.6',
+        number: '5000+',
+        price: '360',
+        red: '新人红包',
+        id: 3
+      },
+      {
+        picture: '',
+        position: "南锣鼓巷附近",
+        title: '锦江之星（北京王府井店）',
+        score: '4.6',
+        number: '5000+',
+        price: '360',
+        red: '新人红包',
+        id: 4
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
