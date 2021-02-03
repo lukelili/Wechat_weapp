ff4a661ebd34016d66420937f34d9930cb63fddd
lipeng <lipeng5231@163.com>
2021-02-03 13:50:16 +0800

    s

--- pages/home/city/index.js
+++ pages/home/city/index.js
@@ -1,5 +1,7 @@
 
 import { citys } from '../../../utils/city'
+import Toast from '@vant/weapp/toast/toast'
+import pinyin from 'js-pinyin'
 Page({
 
   /**
@@ -7,21 +9,59 @@ Page({
    */
   data: {
     cityList: citys.cityList,
-    isShowModel: true
+    indexList: [],
+    cities: [],
+    hiddenModel: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // console.log(this.data.cityList)
+    // 初始化 => 城市索引 汉字转化拼音
+    this.init()
   },
-  onSearchChange(e) {
-    const { detail } = e;
-    let isShow = detail.length ? false : true
+  init() {
+    pinyin.setOptions({checkPolyphone: false, charCase: 0});
+    const indexs = this.data.cityList.map(item => item.idx)
     this.setData({
-      isShowModel: isShow
+      indexList: indexs
     })
+    console.log(this.data.cityList)
+  },
+  // 城市索引选中
+  handleIndexSelect(e) {
+    Toast({
+      message: e.detail,
+      duration: 1000
+    })
+  },
+  // 搜索输入框
+  onSearchChange(e) {
+    const value = e.detail
+    if (!value.length) {
+      this.setData({
+        hiddenModel: true
+      })
+      return
+    }
+    this.setData({
+      hiddenModel: false
+    })
+    // 存储值的首位
+    let first = ''
+    const cityList = this.data.cityList
+    let sliceValue = value.slice(0, 1)
+    first = sliceValue.toUpperCase()
+    if (/^[\u4e00-\u9fa5]*$/.test(sliceValue)) {
+      first = pinyin.getFullChars(sliceValue).slice(0, 1)
+    }
+    const cities = cityList.filter(item => item.idx === first)
+    if (value.length === 1) {
+      this.setData({
+        cities: cities[0].cities
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
